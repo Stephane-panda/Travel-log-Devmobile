904200fc03cd31f175f17d3c5e4c34463101fb45
Ingrid Sorg <ingrid.sorgpacheco@heig-vd.ch>
2021-01-15 16:44:31 +0100

    Display resource

---
+++ Travel-log-devmobile/src/app/components/trip/trip.component.ts
@@ -0,0 +1,16 @@
+import { Component,Input, OnInit } from '@angular/core';
+import { Trip } from 'src/app/models/trip';
+
+@Component({
+  selector: 'app-trip',
+  templateUrl: './trip.component.html',
+  styleUrls: ['./trip.component.scss'],
+})
+export class TripComponent implements OnInit {
+  @Input() trip: Trip;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
