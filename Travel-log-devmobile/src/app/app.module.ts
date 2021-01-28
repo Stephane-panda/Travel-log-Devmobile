937f927138519d7968962a8ae2548dab500e6871
Ingrid Sorg <ingrid.sorgpacheco@heig-vd.ch>
2021-01-28 14:10:05 +0100

    css inputs

--- Travel-log-devmobile/src/app/app.module.ts
+++ Travel-log-devmobile/src/app/app.module.ts
@@ -14,9 +14,7 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { IonicStorageModule } from '@ionic/storage';
 
 import { LOCALE_ID } from '@angular/core';
-
 import { registerLocaleData } from '@angular/common';
-
 import localeFr from '@angular/common/locales/fr';
 
 registerLocaleData(localeFr);
