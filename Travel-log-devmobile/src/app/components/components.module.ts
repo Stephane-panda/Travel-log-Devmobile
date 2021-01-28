0a19ef64401d5e76be865642849221cc24890ab0
Ingrid Sorg <ingrid.sorgpacheco@heig-vd.ch>
2021-01-28 13:32:11 +0100

    flèche

--- Travel-log-devmobile/src/app/components/components.module.ts
+++ Travel-log-devmobile/src/app/components/components.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TripComponent } from './trip/trip.component';
 import  { UserTripComponent } from './user/user.component';
+import { IonicModule } from '@ionic/angular';
+
 
 
 @NgModule({
   declarations: [TripComponent, UserTripComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
   ],
   exports: [TripComponent, UserTripComponent]
 })
