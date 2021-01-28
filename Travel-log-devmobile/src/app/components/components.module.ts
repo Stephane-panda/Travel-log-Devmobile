87617f5402eed89af0afccaf51283778caac2e14
Ingrid Sorg <ingrid.sorgpacheco@heig-vd.ch>
2021-01-28 09:23:53 +0100

    merge avec julie

--- Travel-log-devmobile/src/app/components/components.module.ts
+++ Travel-log-devmobile/src/app/components/components.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TripComponent } from './trip/trip.component';
 
+
+
 @NgModule({
   declarations: [TripComponent],
   imports: [
     CommonModule
   ],
   exports: [TripComponent]
-})
+})  
 export class ComponentsModule { }
