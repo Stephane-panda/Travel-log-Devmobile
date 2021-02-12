ce0eeb8ecc72a30e85a4e0198f04f334bb3a2f15
Stephane-panda <stephane.bondabelo@heig-vd.ch>
2021-02-12 22:11:37 +0100

    start place ts

--- Travel-log-devmobile/src/app/layout/create-place/create-place.page.ts
+++ Travel-log-devmobile/src/app/layout/create-place/create-place.page.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CreatePlaceRequest } from 'src/app/models/Creat-place-request';
 @Component({
-  selector: 'app-create-place-start',
+  selector: 'app-create-place',
   templateUrl: './create-place.page.html',
   styleUrls: ['./create-place.page.scss'],
 })
 export class CreatePlacePage implements OnInit {
-
+createplaceRequest: partial<CreatePlaceRequest> = { title: '', description: '' }
   constructor() { }
 
   ngOnInit() {
