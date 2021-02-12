d5c0892d3405d492f0acce5ccad0cfef8050d508
Stephane-panda <stephane.bondabelo@heig-vd.ch>
2021-02-12 19:06:47 +0100

    place services

--- Travel-log-devmobile/src/app/services/place.service.ts
+++ Travel-log-devmobile/src/app/services/place.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Place } from '../models/Place';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpParams} from '@angular/common/http';
+
+import { CreatePlaceRequest } from 'src/app/models/Creat-place-request';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlaceService {
 
-  constructor() { }
-}
+  constructor(private http: HttpClient) { }
+
+  createPlace(createplaceRequest: CreatePlaceRequest) {
+
+    return this.http.post(environment.apiUrl + '/places', createplaceRequest);
+}}
