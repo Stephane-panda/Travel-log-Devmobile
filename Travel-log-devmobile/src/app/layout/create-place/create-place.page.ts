1c5d963195e4ceb95a4a4bde65b7176d5d3e9d39
Stephane-panda <stephane.bondabelo@heig-vd.ch>
2021-02-14 19:18:30 +0100

    pitcure dont  work

--- Travel-log-devmobile/src/app/layout/create-place/create-place.page.ts
+++ Travel-log-devmobile/src/app/layout/create-place/create-place.page.ts
@@ -2,17 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { CreatePlaceRequest } from 'src/app/models/Creat-place-request';
 import { Router } from '@angular/router';
 import { PlaceService } from 'src/app/services/place.service';
+import { QimgImage } from '../../models/qimg-image';
+/* import { PictureProvider } from '../../services/picture/picture.service'; */
+/* import { Camera, CameraOptions } from '@ionic-native/camera/ngx'; */
 @Component({
   selector: 'app-create-place',
   templateUrl: './create-place.page.html',
   styleUrls: ['./create-place.page.scss'],
 })
 export class CreatePlacePage implements OnInit {
-createPlaceRequest: CreatePlaceRequest = {name: '', description: '', location : '[46.453377, 6.893891]', tripHref : '/api/trips/4906251d-9909-46b6-8973-25ab690329df', tripId : '4906251d-9909-46b6-8973-25ab690329df', pictureUrl: '' };
-  constructor(
+constructor(
     private placeService: PlaceService,
-    private router: Router
-    ) { }
+    private router: Router,
+/*     private pictureService: PictureProvider, */
+    /* private camera: Camera */ ) { }
+createPlaceRequest: CreatePlaceRequest = {name: '', description: '', location : '[46.453377, 6.893891]', tripHref : '/api/trips/4906251d-9909-46b6-8973-25ab690329df', tripId : '4906251d-9909-46b6-8973-25ab690329df', pictureUrl: '' };
+picture: QimgImage;
+
+
+
+
 
     savePlace() {
     this.placeService.createPlace(this.createPlaceRequest).subscribe((res) => {
@@ -20,6 +29,28 @@ createPlaceRequest: CreatePlaceRequest = {name: '', description: '', location : 
     });
   }
 
+ takePicture() {
+
+  /* const options: CameraOptions = {
+  quality: 100,
+  destinationType: this.camera.DestinationType.FILE_URI,
+  encodingType: this.camera.EncodingType.JPEG,
+  mediaType: this.camera.MediaType.PICTURE
+};
+
+  this.camera.getPicture(options).then((imageData) => {
+ // imageData is either a base64 encoded string or a file URI
+ // If it's base64 (DATA_URL):
+ const base64Image = 'data:image/jpeg;base64,' + imageData;
+}, (err) => {
+ // Handle error
+}); */
+/*   this.pictureService.takeAndUploadPicture().subscribe(picture => {
+      this.picture = picture;
+    }, err => {
+      console.warn('Could not take picture', err);
+    }); */
+  }
   ngOnInit() {
   }
 
--- Travel-log-devmobile/src/app/services/user.service.ts
+++ Travel-log-devmobile/src/app/services/user.service.ts
@@ -3,8 +3,9 @@ import { AuthService } from 'src/app/auth/auth.service';
 import { TripService } from 'src/app/services/trip.service';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import {catchError, map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -26,10 +27,26 @@ export class UserService {
   }
 
   changeName(newName, userId): Observable<User> {
-    return this.http.patch<User>(environment.apiUrl + '/users/' + userId, newName);
+    return this.http.patch<User>(environment.apiUrl + '/users/' + userId, newName)
+    .pipe(
+        catchError((err) => {
+          console.log('error caught in service');
+          console.error(err);
+          // Handle the error here
+          return throwError(err);    // Rethrow it back to component
+        })
+      );
   }
 
   deleteUser(userId) {
-    return this.http.delete<User>(environment.apiUrl + '/users/' + userId);
+    return this.http.delete<User>(environment.apiUrl + '/users/' + userId)
+      .pipe(
+        catchError((err) => {
+          console.log('error caught in service');
+          console.error(err);
+          // Handle the error here
+          return throwError(err);    // Rethrow it back to component
+        })
+      );
   }
 }
